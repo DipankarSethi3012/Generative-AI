3807225a7de916ef3c72a5ebf8c3c058d0bc0977
DipankarSethi3012 <dipankarsethi3012@gmail.com>
2026-05-18 20:44:19 +0530

    Setting up RAg Project, Adding Dependencies

---
+++ firstrag/firstrag/src/main/java/com/rag/firstrag/FirstragApplication.java
@@ -0,0 +1,13 @@
+package com.rag.firstrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstragApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstragApplication.class, args);
+	}
+
+}
